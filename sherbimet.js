a3960c51b7546cba45e3b5a8a851b206afe0ff9d
hotifestim22-byte <hotifestim22@gmail.com>
2026-07-22 09:27:36 +0200

    new

--- sherbimet.js
+++
@@ -1,9 +0,0 @@
-    const currentPage = window.location.pathname;
-    const navLinks = document.querySelectorAll('nav a');
-    
-    navLinks.forEach(link => {
-        const href = link.getAttribute('href');
-        if (currentPage.includes(href)) {
-            link.classList.add('active');
-        }
-    });
